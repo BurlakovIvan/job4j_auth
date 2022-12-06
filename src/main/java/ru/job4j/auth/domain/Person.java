efb2333eb0ba66186855c8044f33dcc8d49dcc27
BurlakovIvan <burlakovid@inbox.ru>
2022-12-06 22:16:07 +0300

    RESTFull. Описание архитектуры

---
+++ src/main/java/ru/job4j/auth/domain/Person.java
@@ -0,0 +1,21 @@
+package ru.job4j.auth.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private String login;
+    private String password;
+}
